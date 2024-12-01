56c86570184295421310041d087a3550d2fd55b6
jserrats <jaume.serrats9@gmail.com>
2024-12-01 12:11:39 +0100

    deploying changes

--- src/index.ts
+++ src/index.ts
@@ -1,88 +1,83 @@
 
 
 import 'dotenv/config'
-import { router } from "mqtt-assistant";
-import { zigbee, esphome, telegram, Timer, Sun, Alarm, Weather, assistant } from "mqtt-assistant"
-import { TemperatureLightOptions } from 'mqtt-assistant/dist/components/zigbee/light';
+import { zigbee, esphome, telegram, Timer, Sun, Alarm, assistant } from "mqtt-assistant"
+import { globalEventManager } from 'mqtt-assistant/dist/components/component'
 
 console.log("[i] Starting Automations")
 telegram.info("Starting Automations")
 
-// Misc
+// // Misc
 
-new zigbee.ZigbeeMonitor(["music_light"])
+new zigbee.ZigbeeMonitor()
 new esphome.EsphomeMonitor()
 
-// Living Room
-var livingroomRemote = new zigbee.RemoteE2002("livingroom_remote")
-var livingroomSmoothLights = new zigbee.PowerE1603("livingroom_smooth_lights")
+// // Living Room
+var livingroomRemote = new zigbee.remotes.RemoteE2002("livingroom_remote")
+var livingroomSmoothLights = new zigbee.switches.E1603("livingroom_smooth_lights")
 var clock = new esphome.LightESPHome("minimatrix", "clock")
 var livingRoomClockTimer = new Timer({ hours: 8 })
-livingRoomClockTimer.on('timeout', () => { clock.setOn() })
-livingRoomClockTimer.addCancelTriggers(livingroomRemote.trigger.holdDown)
-livingRoomClockTimer.on('cancel', () => { clock.setOn() })
-router.addAutomation({ trigger: livingroomRemote.trigger.up, callback: () => { livingroomSmoothLights.toggle() } })
-router.addAutomation({
-    trigger: livingroomRemote.trigger.down,
-    callback: () => {
-        clock.setOff()
-        livingRoomClockTimer.start()
-    }
+
+globalEventManager.on([livingRoomClockTimer.events.timeout, livingRoomClockTimer.events.cancel], () => { clock.setOn() })
+livingroomRemote.on(livingroomRemote.button.holdDown, () => { livingRoomClockTimer.cancel() })
+livingroomRemote.on(livingroomRemote.button.up, () => { livingroomSmoothLights.toggle() })
+livingroomRemote.on(livingroomRemote.button.down, () => {
+    clock.setOff()
+    livingRoomClockTimer.start()
 })
 
-// Lobby
-var lobbyLight = new zigbee.LightLED1623G12("lobby_light")
+// // Lobby
+// var lobbyLight = new zigbee.lights.LED1623G12("lobby_light")
 
 // Workshop
-var workshopPower = new zigbee.PowerE1603("workshop_power") // TODO: { autoOff: { hours: 4 } })
-var workshopRemote = new zigbee.RemoteE1812("workshop_remote")
-router.addAutomation({ trigger: workshopRemote.trigger.click, callback: () => { workshopPower.toggle() } })
+var workshopPower = new zigbee.switches.E1603("workshop_power") // TODO: { autoOff: { hours: 4 } })
+var workshopRemote = new zigbee.remotes.RemoteE1812("workshop_remote")
+workshopRemote.on(workshopRemote.button.click, () => { workshopPower.toggle() })
 
 // Laundry room
-var laundrySensor = new zigbee.PresenceSensorZigbee("laundry_presence")
-var laundryLight = new zigbee.LightZigbee("laundry_light")
-router.addAutomation({ trigger: laundrySensor.trigger.occupied, callback: () => { laundryLight.setOn() } })
-router.addAutomation({ trigger: laundrySensor.trigger.cleared, callback: () => { laundryLight.setOff() } })
+var laundrySensor = new zigbee.sensors.presence.IH012_RT01("laundry_presence")
+var laundryLight = new zigbee.lights.LED1623G12("laundry_light")
+laundrySensor.occupancy.on(laundrySensor.occupancy.events.state, (state) => { if (state) { laundryLight.setOn() } else { laundryLight.setOff() } })
 
 // Music
 
-var musicRemote = new zigbee.RemoteE2201("music_remote")
-var musicMoodLight = new zigbee.LightLED1623G12("mood_music_light")
+var musicRemote = new zigbee.remotes.RemoteE2201("music_remote")
+var musicMoodLight = new zigbee.lights.LED1623G12("mood_music_light")
 
-router.addAutomation({ trigger: musicRemote.trigger.topClick, callback: () => { musicMoodLight.toggle() } })
-router.addAutomation({ trigger: musicRemote.trigger.bottomClick, callback: () => { musicMoodLight.setOn(brighterWarmLight) } })
-router.addAutomation({ trigger: musicRemote.trigger.holdBottomClick, callback: () => { musicMoodLight.setOn(dayLight) } })
+musicRemote.on(musicRemote.button.topClick, () => { musicMoodLight.toggle() })
+musicRemote.on(musicRemote.button.bottomClick, () => { musicMoodLight.setOn(brighterWarmLight) })
+musicRemote.on(musicRemote.button.holdBottomClick, () => { musicMoodLight.setOn(dayLight) })
+
 
 // Studio
 var studioPresence = new esphome.BinarySensorESPHome("datacenter", "studio_presence")
-var studioLight = new zigbee.LightLED1623G12("studio_light")
-var studioFan = new zigbee.PowerE1603("studio_fan")
-var deskPower = new zigbee.PowerE1603("desk_power")
-var shelvesLight = new zigbee.LightZigbee("studio_shelf_light")
+var studioLight = new zigbee.lights.LED1623G12("studio_light")
+var studioFan = new zigbee.switches.E1603("studio_fan")
+var deskPower = new zigbee.switches.E1603("desk_power")
+var shelvesLight = new zigbee.lights.YSR_MINI_01_dimmer("studio_shelf_light")
 
-var deskTimer = new Timer({ minutes: 5 })
-deskTimer.on('timeout', () => {
+var deskTimer = new Timer({ minutes: 10 })
+
+deskTimer.on(deskTimer.events.timeout, () => {
     deskPower.setOff()
-    shelvesLight.setBrightness(100)
+    shelvesLight.brightness.set(100)
 })
-deskTimer.addCancelTriggers(studioPresence.trigger.on)
 
-var shelvesLightTimer = new Timer({ minutes: 10 })
-shelvesLightTimer.on('timeout', () => {
+var shelvesLightTimer = new Timer({ minutes: 20 })
+
+shelvesLightTimer.on(shelvesLightTimer.events.timeout, () => {
     shelvesLight.setOff()
     studioFan.setOff()
 })
-shelvesLightTimer.addCancelTriggers(studioPresence.trigger.on)
 
-router.addAutomation({
-    trigger: studioPresence.trigger.on, callback: () => {
+studioPresence.on(studioPresence.events.state, (state) => {
+    if (state) {
+        deskTimer.cancel();
+        shelvesLightTimer.cancel()
         studioLight.setOn()
         deskPower.setOn()
         shelvesLight.setOn({ brightness: 180 })
-    }
-})
-router.addAutomation({
-    trigger: studioPresence.trigger.off, callback: () => {
+    } else {
         studioLight.setOff()
         deskTimer.start()
         shelvesLightTimer.start()
@@ -90,81 +85,80 @@ router.addAutomation({
 })
 
 // Bedroom
-var bedroomRemoteLeft = new zigbee.RemoteTS0044("bedroom_left_remote")
-var bedroomRemoteRight = new zigbee.RemoteTS0044("bedroom_right_remote")
+var bedroomRemoteLeft = new zigbee.remotes.RemoteTS0044("bedroom_left_remote")
+var bedroomRemoteRight = new zigbee.remotes.RemoteTS0044("bedroom_right_remote")
 
-var bedroomFan = new zigbee.PowerE1603("bedroom_fan")
+var bedroomFan = new zigbee.switches.E1603("bedroom_fan")
 var bedroomFanTimer = new Timer({ minutes: 30 }, "bedroom_fan")
-bedroomFanTimer.on('timeout', () => {
+
+bedroomFanTimer.on(bedroomFanTimer.events.timeout, () => {
     bedroomFan.setOff()
 })
-bedroomFanTimer.addCancelTriggers([
-    bedroomRemoteLeft.trigger.bottomLeftHold,
-    bedroomRemoteRight.trigger.bottomLeftHold,
-])
 
-// fan control
-router.addAutomation({
-    trigger: [
-        bedroomRemoteLeft.trigger.bottomLeftSingleClick,
-        bedroomRemoteRight.trigger.bottomLeftSingleClick
-    ], callback: () => {
+globalEventManager.on(
+    [
+        bedroomRemoteLeft.button.bottomLeftHold,
+        bedroomRemoteRight.button.bottomLeftHold
+    ], () => { bedroomFanTimer.cancel() })
+
+globalEventManager.on(
+    [
+        bedroomRemoteLeft.button.bottomLeftSingleClick,
+        bedroomRemoteRight.button.bottomLeftSingleClick
+    ], () => {
         bedroomFan.toggle();
         bedroomFanTimer.cancel()
-    }
-})
+    })
 
-router.addAutomation({
-    trigger: [
-        bedroomRemoteLeft.trigger.bottomLeftDoubleClick,
-        bedroomRemoteRight.trigger.bottomLeftDoubleClick
-    ], callback: () => {
+globalEventManager.on(
+    [
+        bedroomRemoteLeft.button.bottomLeftDoubleClick,
+        bedroomRemoteRight.button.bottomLeftDoubleClick
+    ], () => {
         bedroomFan.setOn()
         bedroomFanTimer.start()
-    }
-})
+    })
 
 // lights
 
-const dayLight = { brightness: 254, color_temp: 250 } as TemperatureLightOptions
-const warmLight = { brightness: 5, color_temp: 450 } as TemperatureLightOptions
-const brighterWarmLight = { brightness: 218, color_temp: 450 } as TemperatureLightOptions
+const dayLight = { brightness: 254, color_temp: 250 }
+const warmLight = { brightness: 5, color_temp: 450 }
+const brighterWarmLight = { brightness: 218, color_temp: 450 }
 
-var bedroomLightLeft = new zigbee.LightLED1623G12("bedroom_left_light")
-var bedroomLightRight = new zigbee.LightLED1623G12("bedroom_right_light")
-var bedroomMoodLight = new zigbee.PowerZigbee("bedroom_mood_light")
-var bedroomRemoteEntrance = new zigbee.RemoteE1812("bedroom_remote")
+var bedroomLightLeft = new zigbee.lights.LED1623G12("bedroom_left_light")
+var bedroomLightRight = new zigbee.lights.LED1623G12("bedroom_right_light")
+var bedroomMoodLight = new zigbee.switches.E1603("bedroom_mood_light")
+var bedroomRemoteEntrance = new zigbee.remotes.RemoteE1812("bedroom_remote")
 var nightStandLight = new esphome.LightESPHome("bedroom", "nightstand_led")
 
-router.addAutomation({
-    trigger: [bedroomRemoteLeft.trigger.topLeftHold, bedroomRemoteRight.trigger.topLeftHold], callback: () => {
-        nightStandLight.setOff()
-    }
-})
+globalEventManager.on(
+    [
+        bedroomRemoteLeft.button.topLeftHold,
+        bedroomRemoteRight.button.topLeftHold
+    ], () => { nightStandLight.setOff() })
 
-router.addAutomation({
-    trigger: [
-        bedroomRemoteLeft.trigger.bottomRightSingleClick,
-        bedroomRemoteRight.trigger.bottomRightSingleClick,
-        bedroomRemoteEntrance.trigger.click
+globalEventManager.on(
+    [
+        bedroomRemoteRight.button.bottomRightSingleClick,
+        bedroomRemoteLeft.button.bottomRightSingleClick,
+        bedroomRemoteEntrance.button.click
     ],
-    callback: () => {
+    () => {
         if (bedroomLightLeft.state || bedroomLightRight.state) {
-            bedroomLightLeft.setOff();
-            bedroomLightRight.setOff()
+            bedroomLightLeft.setOff(),
+                bedroomLightRight.setOff()
         } else {
-            bedroomLightLeft.setOn(dayLight);
-            bedroomLightRight.setOn(dayLight)
+            bedroomLightLeft.setOn(dayLight),
+                bedroomLightRight.setOn(dayLight)
         }
-    }
-})
+    })
 
-router.addAutomation({
-    trigger: [
-        bedroomRemoteLeft.trigger.topLeftSingleClick,
-        bedroomRemoteRight.trigger.topLeftSingleClick,
+globalEventManager.on(
+    [
+        bedroomRemoteRight.button.topLeftSingleClick,
+        bedroomRemoteLeft.button.topLeftSingleClick
     ],
-    callback: () => {
+    () => {
         if (bedroomMoodLight.state) {
             bedroomMoodLight.setOff()
         } else {
@@ -172,51 +166,40 @@ router.addAutomation({
             bedroomLightRight.setOff()
             bedroomLightLeft.setOff()
         }
-    }
-})
+    })
 
-router.addAutomation({
-    trigger: [
-        bedroomRemoteLeft.trigger.bottomRightDoubleClick,
-        bedroomRemoteRight.trigger.bottomRightDoubleClick,
-    ],
-    callback: () => {
+globalEventManager.on(
+    [
+        bedroomRemoteRight.button.bottomRightDoubleClick,
+        bedroomRemoteLeft.button.bottomRightDoubleClick
+    ], () => {
         bedroomLightLeft.setOn(warmLight),
             bedroomLightRight.setOn(warmLight)
-    }
+    })
+
+
+
+bedroomRemoteRight.on(bedroomRemoteRight.button.bottomRightHold, () => {
+    bedroomLightLeft.setOff()
+    bedroomLightRight.setOn(warmLight)
+})
+bedroomRemoteLeft.on(bedroomRemoteLeft.button.bottomRightHold, () => {
+    bedroomLightRight.setOff()
+    bedroomLightLeft.setOn(warmLight)
 })
 
-router.addAutomation({
-    trigger: [
-        bedroomRemoteRight.trigger.bottomRightHold,
-    ],
-    callback: () => {
-        bedroomLightLeft.setOff()
-        bedroomLightRight.setOn(warmLight)
-    }
-})
 
-router.addAutomation({
-    trigger: [
-        bedroomRemoteLeft.trigger.bottomRightHold,
-    ],
-    callback: () => {
-        bedroomLightRight.setOff()
-        bedroomLightLeft.setOn(warmLight)
-    }
-})
+// // mosquito
 
-// mosquito
-
-var mosquitoRepellant = new zigbee.PowerE1603("mosquito_power")
+var mosquitoRepellant = new zigbee.switches.E1603("mosquito_power")
 var mosquitoTimer = new Timer({ hours: 8 }, "mosquito")
-mosquitoTimer.on('timeout', () => { mosquitoRepellant.setOff() })
-router.addAutomation({
-    trigger: [
-        bedroomRemoteLeft.trigger.topRightSingleClick,
-        bedroomRemoteRight.trigger.topRightSingleClick,
-    ],
-    callback: () => {
+mosquitoTimer.on(mosquitoTimer.events.timeout, () => { mosquitoRepellant.setOff() })
+
+globalEventManager.on(
+    [
+        bedroomRemoteRight.button.topRightSingleClick,
+        bedroomRemoteLeft.button.topRightSingleClick
+    ], () => {
         if (mosquitoRepellant.state) {
             mosquitoRepellant.setOff()
             mosquitoTimer.cancel()
@@ -224,37 +207,26 @@ router.addAutomation({
             mosquitoRepellant.setOn()
             mosquitoTimer.start()
         }
-    }
-})
+    })
 
-// Kitchen
+// // Kitchen
 var sandwich = new esphome.SwitchESPHome("sandwich", "sandwich")
 var sandwichTimer = new Timer({ minutes: 5 }, "sandwich")
-sandwichTimer.on('timeout', () => { sandwich.setOff() })
-sandwichTimer.addCancelTriggers(sandwich.trigger.off)
-const airfryer_power = new zigbee.PowerSensorZigbee("airfryer_power")
-const airfryer_binary = new assistant.CustomBinarySensor<number>("airfryer_binary", (value: number) => {
-    return (value > 10)
+sandwichTimer.on(sandwichTimer.events.timeout, () => { sandwich.setOff() })
+sandwich.on(sandwich.events.state, () => { if (sandwich.state) { sandwichTimer.start() } else { sandwichTimer.cancel() } })
+const airfryer_power = new zigbee.switches.BSD29_1("airfryer_power")
+const airfryer_binary = new assistant.CustomSensor<boolean>("airfryer_binary", airfryer_power.power, (value) => {
+    return (value as number > 10)
 })
-
-airfryer_power.on('state', (value: number) => { airfryer_binary.updateComponent(value) })
-
-router.addAutomation({
-    trigger: sandwich.trigger.on,
-    callback: () => {
-        sandwichTimer.start()
-    }
-})
-
-// weather
+// // weather
 
 new Sun(41.3831173, 2.1640883)
-new Weather(41.3831173, 2.1640883)
+// new Weather(41.3831173, 2.1640883)
 
-// alarm
+// // alarm
 
-var door = new zigbee.ClosureSensorZigbee("door_closure_sensor", { inverted: true })
-var window1 = new zigbee.ClosureSensorZigbee("studio_window_closure_sensor")
-var window2 = new zigbee.ClosureSensorZigbee("music_window_closure_sensor")
+var door = new zigbee.sensors.closure.TS0203("door_closure_sensor", { inverted: true })
+var window1 = new zigbee.sensors.closure.TS0203("studio_window_closure_sensor")
+var window2 = new zigbee.sensors.closure.TS0203("music_window_closure_sensor")
 
-new Alarm("home", [door, window1, window2])
+new Alarm("home", [door.contact, window1.contact, window2.contact])
